e9978dd45e4e12ec2a4fe427ec7143d011f1fdd0
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-07-21 15:03:55 -0300

    add: @JoinColumn()

--- src/main/java/com/diogo/client_banking_api/model/Client.java
+++ src/main/java/com/diogo/client_banking_api/model/Client.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -24,5 +25,6 @@ public class Client {
     private String email;
 
     @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id")
     private BankAccount bankAccount;
 }
