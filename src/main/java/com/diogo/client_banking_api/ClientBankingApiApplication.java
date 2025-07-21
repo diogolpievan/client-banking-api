d132ba3223fec86b6086c1c95d444a396a280fcc
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-07-21 18:07:56 -0300

    add: enableFeignClients

--- src/main/java/com/diogo/client_banking_api/ClientBankingApiApplication.java
+++ src/main/java/com/diogo/client_banking_api/ClientBankingApiApplication.java
@@ -2,8 +2,10 @@ package com.diogo.client_banking_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ClientBankingApiApplication {
 
 	public static void main(String[] args) {
