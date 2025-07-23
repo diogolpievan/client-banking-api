e0bb9337e13dfcf8cdae85295ef36577173ba460
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-07-23 12:24:53 -0300

    feat: controller

---
+++ src/main/java/com/diogo/client_banking_api/controller/ClientController.java
@@ -0,0 +1,36 @@
+package com.diogo.client_banking_api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.diogo.client_banking_api.dto.ClientRequestDTO;
+import com.diogo.client_banking_api.dto.ClientResponseDTO;
+import com.diogo.client_banking_api.service.ClientService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/clients")
+@RequiredArgsConstructor
+public class ClientController {
+    
+    private final ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<ClientResponseDTO> createClient(@RequestBody ClientRequestDTO request){
+        ClientResponseDTO response = clientService.createClient(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientResponseDTO> getClientById(@PathVariable Long id){
+        ClientResponseDTO client = clientService.getClientById(id);
+        return ResponseEntity.ok(client);
+    }
+}
