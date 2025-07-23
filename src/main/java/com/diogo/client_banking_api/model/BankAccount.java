2ec1bdf136d2616ebd2041277f1adfc9dc86f714
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-07-23 11:26:24 -0300

    fix: fix Client FK for BankAccount

--- src/main/java/com/diogo/client_banking_api/model/BankAccount.java
+++ src/main/java/com/diogo/client_banking_api/model/BankAccount.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,4 +30,8 @@ public class BankAccount {
     @ManyToOne
     @JoinColumn(name = "bank_code")
     private Bank bank;
+    
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
 }
--- src/main/java/com/diogo/client_banking_api/model/Client.java
+++ src/main/java/com/diogo/client_banking_api/model/Client.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -24,7 +23,6 @@ public class Client {
     private String name;
     private String email;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "bank_account_id")
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
     private BankAccount bankAccount;
 }
