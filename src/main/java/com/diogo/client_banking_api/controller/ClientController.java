2c2677e9137ee003ca14d7879a33583133b81100
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-07-23 14:54:40 -0300

    finish: ClientBankingAPI finished

--- src/main/java/com/diogo/client_banking_api/controller/ClientController.java
+++ src/main/java/com/diogo/client_banking_api/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.diogo.client_banking_api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.diogo.client_banking_api.dto.ClientRequestDTO;
 import com.diogo.client_banking_api.dto.ClientResponseDTO;
 import com.diogo.client_banking_api.service.ClientService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
